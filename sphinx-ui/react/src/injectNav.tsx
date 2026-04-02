22d573a8d48f18ff8b07cfc45c0fa6ff7d77e65f
Sean Burton <sean.burton@quantinuum.com>
2026-04-02 17:24:55 +0100

    Fixes to analytics and navbar, move from unmaintained tsup to tsdown Also fix path to favicon

--- sphinx-ui/react/src/injectNav.tsx
+++ sphinx-ui/react/src/injectNav.tsx
@@ -1,7 +1,10 @@
 
 import {createRoot} from "react-dom/client" 
-import { DocsNavBar, CookieCategoryName, CookieConsentProvider, CookieConsentManager } from "@quantinuum/documentation-ui";
+import { DocsNavBar, CookieCategoryName, CookieConsentProvider, CookieConsentManager, CookieConditional } from "@quantinuum/documentation-ui";
+import { GoogleAnalytics } from '@next/third-parties/google';
 import { ComponentProps } from "react"; // do not remove
+
+const GA_ID = __NEXT_PUBLIC_GA_ID__;
 
   (() => {
     const mountElement = document.querySelector('.nexus-nav')
@@ -14,11 +17,10 @@ import { ComponentProps } from "react"; // do not remove
     root.render(
       <div className="use-tailwind">
         <div className="antialiased" style={{fontFamily: `Inter, ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`}}>
-          <CookieConsentProvider>
+          <CookieConsentProvider version={1}>
             <CookieConditional category={CookieCategoryName.Analytics} fallback={null}>
-              <AnalyticsScript />
+              {GA_ID && <GoogleAnalytics gaId={GA_ID} />}
             </CookieConditional>
-
             <DocsNavBar activePath="/" />
             <CookieConsentManager />
           </CookieConsentProvider>
