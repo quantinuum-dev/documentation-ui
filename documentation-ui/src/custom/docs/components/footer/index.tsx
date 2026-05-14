97181aa0610ffc85629c61826bbc6659e96a7b82
Sean Burton <sean.burton@quantinuum.com>
2026-05-14 13:33:16 +0100

    fix: fix footer column layout (#15)

--- documentation-ui/src/custom/docs/components/footer/index.tsx
+++ documentation-ui/src/custom/docs/components/footer/index.tsx
@@ -100,7 +100,7 @@ export const Footer = () => {
           </a><div>/</div>
         </div>
       </div>
-      <div className="grid grid-cols-1 gap-16 md:grid-cols-3 md:gap-24">
+      <div className="grid grid-cols-1 gap-16 sm:grid-cols-2 md:grid-cols-4 md:gap-24">
         {footerConfig.columns.map((col) => {
           return (
             <div key={col.name} className="flex flex-col md:items-end">
