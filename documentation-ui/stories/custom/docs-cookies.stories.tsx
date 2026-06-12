8ff59bc15c66f81609a3243be89a9ed2df725138
Sean Burton <sean.burton@quantinuum.com>
2026-06-12 17:37:25 +0100

    feat: update Storybook to v10 (#40)

--- documentation-ui/stories/custom/docs-cookies.stories.tsx
+++ documentation-ui/stories/custom/docs-cookies.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from '@storybook/react'
+import { Meta, StoryObj } from '@storybook/react-vite'
 import { CookieConsentManager, CookieConsentProvider } from 'src'
 
 export function DocsCookiesDemo() {
