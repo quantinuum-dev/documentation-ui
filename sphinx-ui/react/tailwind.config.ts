e142bc664144aef587b849b81681ba1e22c6344e
aidanCQ <aidan.keay@quantinuum.com>
2026-06-19 17:28:43 +0100

    Attempt fix.

--- sphinx-ui/react/tailwind.config.ts
+++ sphinx-ui/react/tailwind.config.ts
@@ -1,50 +1,45 @@
 /// <reference types="node" />
 
 import path from 'path'
+import { fileURLToPath } from 'url'
 import { Config } from 'tailwindcss'
 import plugin from 'tailwindcss/plugin'
 import { tailwindTheme } from '@quantinuum/documentation-ui/tailwindTheme'
 
-const documentationUiDistSrcDir = path.dirname(require.resolve('@quantinuum/documentation-ui'))
-const quantinuumUiDistSrcDir = path.dirname(require.resolve('@quantinuum/quantinuum-ui'))
+const documentationUiDistSrcDir = path.dirname(fileURLToPath(import.meta.resolve('@quantinuum/documentation-ui')))
+const quantinuumUiDistSrcDir = path.dirname(fileURLToPath(import.meta.resolve('@quantinuum/quantinuum-ui')))
 
 export default {
-    content: [
-        './src/**/*.{js,ts,jsx,tsx,mdx,html}',
-        path.join(
-          documentationUiDistSrcDir,
-          '**/*.{js,ts,jsx,tsx,mdx}'
-        ),
-        path.join(
-          quantinuumUiDistSrcDir,
-          '**/*.{js,ts,jsx,tsx,mdx}'
-        ),
-    ],
-    theme: {
-      extend: {
-        screens: {
-          wide: '1600px',
-        },
-        // fontFamily: {
-        //   'inter': ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
-        // },
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx,html}',
+    path.join(documentationUiDistSrcDir, '**/*.{js,ts,jsx,tsx,mdx}'),
+    path.join(quantinuumUiDistSrcDir, '**/*.{js,ts,jsx,tsx,mdx}'),
+  ],
+  theme: {
+    extend: {
+      screens: {
+        wide: '1600px',
       },
+      // fontFamily: {
+      //   'inter': ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+      // },
     },
-    plugins: [
-        plugin(({ matchUtilities, theme }) => {
-          matchUtilities(
-            {
-              'animation-delay': (value) => {
-                return {
-                  'animation-delay': value,
-                }
-              },
-            },
-            {
-              values: theme('transitionDelay'),
+  },
+  plugins: [
+    plugin(({ matchUtilities, theme }) => {
+      matchUtilities(
+        {
+          'animation-delay': (value) => {
+            return {
+              'animation-delay': value,
             }
-          )
-        }),
-      ],
-      presets: [tailwindTheme]
+          },
+        },
+        {
+          values: theme('transitionDelay'),
+        }
+      )
+    }),
+  ],
+  presets: [tailwindTheme],
 } satisfies Config
